e1f5c90412623e299c5b058ec06248b7203d4bb7
Primus-8 <sy7884212@gmail.com>
2026-06-24 18:30:43 +0530

    update

--- src/pages/StudentDashboard.jsx
+++ src/pages/StudentDashboard.jsx
@@ -1,12 +1,88 @@
+import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
 function StudentDashboard() {
-
   return (
-    <div style={{ padding: "20px" }}>
+    <div>
+      <Navbar />
 
-      <h1>Student Dashboard</h1>
+      <div
+        style={{
+          padding: "30px"
+        }}
+      >
+        <h1>Student Dashboard</h1>
 
-      <p>Welcome Student</p>
+        <p>
+          Welcome Student
+        </p>
 
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns:
+              "repeat(auto-fit, minmax(220px, 1fr))",
+            gap: "20px",
+            marginTop: "30px"
+          }}
+        >
+
+          <Link
+            to="/subjects"
+            style={{
+              textDecoration: "none"
+            }}
+          >
+            <div
+              style={{
+                border: "1px solid #ddd",
+                padding: "25px",
+                borderRadius: "10px",
+                textAlign: "center"
+              }}
+            >
+              <h3>Subjects</h3>
+            </div>
+          </Link>
+
+          <Link
+            to="/topics"
+            style={{
+              textDecoration: "none"
+            }}
+          >
+            <div
+              style={{
+                border: "1px solid #ddd",
+                padding: "25px",
+                borderRadius: "10px",
+                textAlign: "center"
+              }}
+            >
+              <h3>Topics</h3>
+            </div>
+          </Link>
+
+          <Link
+            to="/questions"
+            style={{
+              textDecoration: "none"
+            }}
+          >
+            <div
+              style={{
+                border: "1px solid #ddd",
+                padding: "25px",
+                borderRadius: "10px",
+                textAlign: "center"
+              }}
+            >
+              <h3>Questions</h3>
+            </div>
+          </Link>
+
+        </div>
+      </div>
     </div>
   );
 }
